a0e8c90165e0f0fc152515da1f9dbd4bad338ac5
baliant <39745297+baliant@users.noreply.github.com>
2026-05-04 14:42:17 +0200

    Add default color configuration to config.py

---
+++ src/config.py
@@ -0,0 +1,8 @@
+DEFAULT_COLORS = {
+    "Analog Input": "#FFFF31",
+    "Helyi kijelző": "#FFBF28",
+    "Vezérelt csap": "#8ACF56",
+    "Digital Input": "#00AEED",
+    "Motor": "#FF1318",
+    "Purple": "#ff00ff",
+}
